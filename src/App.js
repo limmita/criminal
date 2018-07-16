3ec37dd477147ee16280ce65b19305843cb14a6a
limmita <akkermanmen@mail.ru>
2018-07-16 14:52:55 +0300

    plus vor v zakone

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import SimpleMap from './components/SimpleMap.js' 
+import vor from './bd.js';
+
+
+
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SimpleMap />
+        <SimpleMap expl = {vor} />
       </div>
     );
   }
--- src/components/SimpleMap.js
+++ src/components/SimpleMap.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react'; 
+import './SimpleMap.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCrown } from '@fortawesome/free-solid-svg-icons'
+library.add(faCrown)
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+
+const AnyReactComponent = ({ text }) => <div className="marker"><FontAwesomeIcon icon="crown" />{text}</div>;
+
 
 class SimpleMap extends Component {
   static defaultProps = {
@@ -9,23 +17,39 @@ class SimpleMap extends Component {
       lat: 46.629829,
       lng: 29.961041
     },
-    zoom: 8
+    zoom: 8,
   };
 
   render() {
+
+    let vorZakon = this.props.expl
+
+
+ var vorElement = vorZakon.map(function(item, i) {
+    // console.log(item);
+      return  item.lacLng.map((itemLaclng,iN) => {
+        console.log(itemLaclng);
+        var N = i + '';
+         return <AnyReactComponent
+            key = {N+iN}
+            lat={itemLaclng[0]}
+            lng={itemLaclng[1]}
+            text={'вор'}
+          />
+        })  
+    })
+    
     return (
-      // Important! Always set the container height explicitly
-      <div clasName="mapFull" style={{ height: '100vh', width: '100%' }}>
+      <div className="mapFull" style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyBoe_CNHgqKQtOvyg8RSwoGnJ7CBEoYRgA' }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <AnyReactComponent
-            lat={46.929829}
-            lng={29.961041}
-            text={'Kreyser Avrora'}
-          />
+        
+        {vorElement}
+          
+
         </GoogleMapReact>
       </div>
     );
@@ -33,3 +57,13 @@ class SimpleMap extends Component {
 }
 
 export default SimpleMap;
+
+// <AnyReactComponent
+//             lat={46.929829}
+//             lng={29.961041}
+//             text={'вор'}
+//           />
+//           <AnyReactComponent
+//             lat={45.473493}
+//             lng={29.243601}
+//             text={'вор'}
